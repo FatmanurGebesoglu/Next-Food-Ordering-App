725127b6447793c9dee9f45913a056169efde96f
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-03-21 12:45:12 +0300

    Header component created with tailwind

---
+++ components/ui/logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const logo = () => {
+  return (
+    <div className='text-[2rem] font-dancing font-bold'>Feane</div>
+  )
+}
+
+export default logo
---
+++ pages/index.jsx
@@ -0,0 +1,18 @@
+import Header from "../components/layout/Header";
+import Head from "next/head";
+import Home from "./home";
+
+export default function Index() {
+  return (
+    <>
+      <Head>
+        <title>Fatmanur</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true"/>
+        
+      </Head>
+      
+      <Header/>
+    </>
+  );
+}
