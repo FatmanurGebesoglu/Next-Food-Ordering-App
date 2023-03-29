ebb5a17b356e16618a82ff03cc7f08a2e0616ed8
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-03-29 12:43:58 +0300

    Input component created

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import Header from "../components/layout/Header";
 import Head from "next/head";
 import Home from "./home";
+import Input from "@/components/form/Input";
 
 export default function Index() {
   return (
@@ -11,9 +12,11 @@ export default function Index() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true"/>
         
       </Head>
-      
       <Header/>
       <Home/>
+      <div className="p-20 container mx-auto">
+          <Input/>
+      </div>
     </>
   );
 }
