18ad0e5a7b9fe7d0d8edfb2260616671a186dacb
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-03-29 15:14:46 +0300

    Reservation component edited

--- components/Reservation.jsx
+++ components/Reservation.jsx
@@ -17,7 +17,7 @@ const Reservation = () => {
           </div>
           <button className="btn-primary mt-4">BOOK NOW</button>
         </div>
-        <div className="lg:flex-1 !h-[384px] w-full">
+        <div className="lg:flex-1 w-full">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3738.2106624920225!2d-73.94301498587603!3d40.68239760298841!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25b8ee5931ba3%3A0xc2ad304bb80d15e0!2sPeaches%20HotHouse!5e0!3m2!1str!2str!4v1680088025262!5m2!1str!2str"
             allowFullScreen=""
