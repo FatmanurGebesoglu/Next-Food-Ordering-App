f8bc1a31b203505f8d0441df0f088ddb7767cf81
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-03-30 15:12:57 +0300

    customers component created

---
+++ components/customers/CustomerItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CustomerItem = () => {
+  return (
+    <div>CustomerItem</div>
+  )
+}
+
+export default CustomerItem
---
+++ components/customers/Customers.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Title } from "../ui/Title";
+
+const Customers = () => {
+  return (
+    <div className="container mx-auto">
+      <Title addclass="text-[40px]">What Says Our Customers</Title>
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Customers;
