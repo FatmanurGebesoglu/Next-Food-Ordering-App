84918cb8ca797fba6c22838724a52b0a412e301d
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-03-30 16:05:09 +0300

    Customer component edited

--- components/customers/Customers.jsx
+++ components/customers/Customers.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Title } from "../ui/Title";
+import CustomerItem from "./CustomerItem";
 
 const Customers = () => {
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto my-20">
       <Title addclass="text-[40px] text-center">What Says Our Customers</Title>
-      <div>
-        <div></div>
-        <div></div>
+      <div className="flex gap-x-10">
+        <CustomerItem imgSrc="/images/client1.jpg" />
+        <CustomerItem imgSrc="/images/client2.jpg" />
       </div>
     </div>
   );
