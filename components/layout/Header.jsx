325464e9660117c453a1634fcfc77fc0b8bb6bea
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-03-31 14:51:39 +0300

    Login page created

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -46,14 +46,14 @@ const Header = () => {
           )}
         </nav>
         <div className="flex gap-x-4 items-center">
-          <Link href="#">
-            <FaUserAlt className="hover:text-primary transition-all" />
+          <Link href="/auth/login">
+            <FaUserAlt className="hover:text-primary transition-all cursor-pointer" />
           </Link>
           <Link href="#">
-            <FaShoppingCart className="hover:text-primary transition-all" />
+            <FaShoppingCart className="hover:text-primary transition-all cursor-pointer" />
           </Link>
           <button onClick={() => setIsSearchModal(true)}>
-            <FaSearch className="hover:text-primary transition-all" />
+            <FaSearch className="hover:text-primary transition-all cursor-pointer" />
           </button>
           
           <Link href="#" className="md:inline-block hidden sm">
