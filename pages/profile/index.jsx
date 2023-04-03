a0af164fdfe8df096cd55704c5653ca9dfb6922b
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-04-03 15:00:26 +0300

    Profile page created

---
+++ pages/profile/index.jsx
@@ -0,0 +1,36 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div className="flex px-10 min-h-[calc(100vh_-_433px)]">
+        <div className="w-80">
+            <div className="relative flex flex-col items-center  p-10 border border-b-0">
+                <Image src="/images/client1.jpg" alt="" width={100} height={100} className="rounded-full" />
+                <b className="text-2xl mt-2">Ellie Miller</b>
+            </div>
+            <ul className="text-center font-semibold">
+                <li className="border w-full p-3 cursor-pointer hover:bg-primary hover:text-white transition-all">
+                    <i className='fa fa-home'></i>
+                    <button className="ml-1">Account</button>
+                </li>
+                <li className="border w-full p-3 cursor-pointer hover:bg-primary hover:text-white transition-all">
+                    <i className='fa fa-key'></i>
+                    <button className="ml-1">Password</button>
+                </li>
+                <li className="border w-full p-3 cursor-pointer hover:bg-primary hover:text-white transition-all">
+                    <i className='fa fa-motorcycle'></i>
+                    <button className="ml-1">Orders</button>
+                </li>
+                <li className="border w-full p-3 cursor-pointer hover:bg-primary hover:text-white transition-all">
+                    <i className='fa fa-sign-out'></i>
+                    <button className="ml-1">Exit</button>
+                </li>
+            </ul>
+        </div>
+        <div></div>
+    </div>
+  )
+}
+
+export default Profile
