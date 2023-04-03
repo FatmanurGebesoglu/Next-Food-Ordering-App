db82fdd5e96206b3a4be7768c8fe3762d413b71e
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-04-03 15:42:26 +0300

    profileSchema edited

--- schema/profileSchema.js
+++ schema/profileSchema.js
@@ -2,13 +2,13 @@ import * as Yup from "yup";
 
 export const profileSchema = Yup.object({
   fullName: Yup.string()
-    .required("Full name is required.")
-    .min(3, "Full name must be at least 3 characters."),
+    .required("İsim Soyisim Zorunludur")
+    .min(3, "İsim Soyisim En Az 3 Karakterden Oluşmalıdır"),
   phoneNumber: Yup.string()
-    .required("Phone Number is required.")
-    .min(10, "Phone number must be at least 10 characters."),
-  email: Yup.string().required("Email is required.").email("Email is invalid."),
-  address: Yup.string().required("Address is required."),
-  job: Yup.string().required("Job is required."),
-  bio: Yup.string().required("Bio is required."),
+    .required("Telefon Numaranızı Giriniz")
+    .min(10, " Telefon Numaranız En Az 10 Karakterden Oluşmalıdır"),
+  email: Yup.string().required("E-Posta Adresinizi Giriniz").email("Geçersiz E-Posta Adresi"),
+  address: Yup.string().required("Adres Zorunludur"),
+  job: Yup.string().required("Meslek Zorunludur"),
+  bio: Yup.string().required("Hakkımda Zorunludur"),
 });
