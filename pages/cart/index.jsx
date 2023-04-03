b0b54c173800696785c62e270becd8e58073e2f3
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-04-03 11:19:27 +0300

    Cart page edited

--- pages/cart/index.jsx
+++ pages/cart/index.jsx
@@ -5,33 +5,33 @@ import React from "react";
 const Cart = () => {
   return (
     <div className="min-h-[calc(100vh_-_433px)]" >
-      <div className="flex justify-between items-center">
-      <div className="min-h-[calc(100vh_-_433px)] flex items-center flex-1 ">
-        <table>
-          <thead>
+      <div className="flex justify-between items-center md:flex-row flex-col">
+      <div className="min-h-[calc(100vh_-_433px)] flex items-center flex-1 p-10 overflow-w-scroll">
+        <table className="w-full txt-sm text-center text-gray-500 min-h-[1000px]">
+          <thead className="text-xs text-gray-400 uppercase bg-gray-700">
             <tr>
-              <th>PRODUCT</th>
-              <th>EXTRAS</th>
-              <th>PRICE</th>
-              <th>QUANTITY</th>
+              <th scope="col" className="py-3 px-6" >PRODUCT</th>
+              <th className="py-3 px-6">EXTRAS</th>
+              <th className="py-3 px-6">PRICE</th>
+              <th className="py-3 px-6">QUANTITY</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>
+            <tr className="bg-secondary border-gray-700 hover:bg-primary transition-all ">
+              <td className=" py-4 px-6 font-medium whitespace-nowrap hover:text-white flex items-center gap-x-1 justify-center">
                 <Image src="/images/pizza.png" alt="" width={50} height={50} />
                 <span>Good Pizza</span>
               </td>
-              <td>
+              <td className=" py-4 px-6 font-medium whitespace-nowrap hover:text-white">
                 <span>mayonez, acı sos, ketçap</span>
               </td>
-              <td>$20</td>
-              <td>1</td>
+              <td className=" py-4 px-6 font-medium whitespace-nowrap hover:text-white">$20</td>
+              <td className=" py-4 px-6 font-medium whitespace-nowrap hover:text-white">1</td>
             </tr>
           </tbody>
         </table>
       </div>
-      <div className="bg-secondary min-h-[calc(100vh_-_433px)] flex flex-col justify-center text-white p-12 ">
+      <div className="bg-secondary min-h-[calc(100vh_-_433px)] flex flex-col justify-center text-white p-12 md:w-auto w-full">
         <Title addclass="text-[40px]" >CART TOTAL</Title>
         <div className="mt-6">
             <b>Subtotal: </b>$20 <br />
