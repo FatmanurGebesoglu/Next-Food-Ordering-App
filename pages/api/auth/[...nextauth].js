28a4d2678c81bf9fb7803ed48005a86a5215b97e
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-04-09 21:48:49 +0300

    Credentials Provider  added for login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,17 +1,53 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import GithubProvider from "next-auth/providers/github"; //github ile giriş yapmak için
+import CredentialsProvider from "next-auth/providers/credentials"; //kendi veritabanımızda kullanıcı olup olmadığını kontrol etmek için
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "@/util/mongo";
+import User from "@/models/User";
+import dbConnect from "@/util/dbConnect";
+import { signIn } from "next-auth/react";
+import bcrypt from "bcryptjs";
+
+dbConnect();
 
 export default NextAuth({
-    adapter: MongoDBAdapter(clientPromise),
+  //adapter: MongoDBAdapter(clientPromise),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const email = credentials.email;
+        const password = credentials.password;
+        const user = await User.findOne({ email: email });
+
+        if (!user) {
+          throw new Error("You are not registered yet!");
+        }
+        if (user) {
+          return signInUser({ user, password });
+        }
+      },
+    }),
   ],
   pages: {
     signIn: "/auth/login",
   },
+  database: process.env.MONGODB_URI,
+  secret :"secret",
 });
+
+const signInUser = async ({ user, password }) => {
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error("Invalid password!");
+  }
+  return user;
+};
