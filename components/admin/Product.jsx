6ee15e5042549df1f36b2a38377add8f1b30d309
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-04-27 12:06:17 +0300

    products called for admin page

--- components/admin/Product.jsx
+++ components/admin/Product.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 import { Title } from "@/components/ui/Title";
 import Image from "next/image";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Product = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get(
+          `${process.env.NEXT_PUBLIC_API_URL}/products`
+        );
+        setProducts(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getProducts();
+  }, []);
+
+  console.log(products);
+
   return (
     <div className="lg:p-8 flex-1 lg:mt-0 mt-5">
-        <Title addclass="text-[40px]">Products</Title>
+      <Title addclass="text-[40px]">Products</Title>
       <div className="overflow-x-auto w-full mt-5">
         <table className="w-full txt-sm text-center text-gray-500 min-w-[1000px]">
           <thead className="text-xs text-gray-400 uppercase bg-gray-700">
@@ -28,23 +48,31 @@ const Product = () => {
             </tr>
           </thead>
           <tbody>
-            <tr className="bg-secondary border-gray-700 hover:bg-primary transition-all ">
-              <td className=" py-4 px-6 font-medium whitespace-nowrap hover:text-white flex items-center gap-x-1 justify-center">
-                <Image src="/images/pizza.png" width={50} height={50}/>
-              </td>
-              <td className=" py-4 px-6 font-medium whitespace-nowrap hover:text-white">
-                <span>343543542</span>
-              </td>
-              <td className=" py-4 px-6 font-medium whitespace-nowrap hover:text-white">
-                <span>Pepperoni Pizza</span>
-              </td>
-              <td className=" py-4 px-6 font-medium whitespace-nowrap hover:text-white">
-                $20
-              </td>
-              <td className=" py-4 px-6 font-medium whitespace-nowrap hover:text-white">
-                <button className="bg-danger text-white px-4 py-2 rounded-md">Delete </button>
-              </td>
-            </tr>
+            {products.length > 0 &&
+              products.map((product) => (
+                <tr
+                  className="bg-secondary border-gray-700 hover:bg-primary transition-all "
+                  key={product._id}
+                >
+                  <td className=" py-4 px-6 font-medium whitespace-nowrap hover:text-white flex items-center gap-x-1 justify-center">
+                    <Image src={product.img} alt={product.title} width={50} height={50} />
+                  </td>
+                  <td className=" py-4 px-6 font-medium whitespace-nowrap hover:text-white">
+                    <span>{product._id.substring(0,5)}...</span>
+                  </td>
+                  <td className=" py-4 px-6 font-medium whitespace-nowrap hover:text-white">
+                    <span>{product.title}</span>
+                  </td>
+                  <td className=" py-4 px-6 font-medium whitespace-nowrap hover:text-white">
+                    ${product.prices[0]}
+                  </td>
+                  <td className=" py-4 px-6 font-medium whitespace-nowrap hover:text-white">
+                    <button className="bg-danger text-white px-4 py-2 rounded-md">
+                      Delete{" "}
+                    </button>
+                  </td>
+                </tr>
+              ))}
           </tbody>
         </table>
       </div>
