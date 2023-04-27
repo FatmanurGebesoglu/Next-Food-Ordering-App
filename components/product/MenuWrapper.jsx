0b5e57cbe4a955265843de113684337bea260afc
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-04-27 11:19:35 +0300

    products called menu

--- components/product/MenuWrapper.jsx
+++ components/product/MenuWrapper.jsx
@@ -1,21 +1,33 @@
 import React from "react";
 import { Title } from "../ui/Title";
 import MenuItem from "./MenuItem";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const MenuWrapper = ({ categoryList, productList }) => {
   const [active, setActive] = useState(0);
+  const [filter, setFilter] = useState([]);
+
+  useEffect(() => {
+    setFilter(
+      productList.filter(
+        (product) =>
+          product.category === categoryList[active].title.toLowerCase()
+      )
+    );
+  }, [categoryList, productList, active]);
+  
+
   return (
-    <div className="container mx-auto mb-16  ">
-      <div className="flex flex-col items-center  w-full ">
+    <div className="container mx-auto  mb-16">
+      <div className="flex flex-col items-center w-full">
         <Title addclass="text-[40px]">Our Menu</Title>
         <div className="mt-10">
           {categoryList &&
             categoryList.map((category, index) => (
               <button
-                className={`px-6 py-2 rounded-3xl ${
-                  index === active && " bg-secondary  text-white"
-                } `}
+                className={`px-6 py-2  rounded-3xl ${
+                  index === active && "bg-secondary text-white"
+                }`}
                 key={category._id}
                 onClick={() => setActive(index)}
               >
@@ -24,11 +36,12 @@ const MenuWrapper = ({ categoryList, productList }) => {
             ))}
         </div>
       </div>
-
-      <div className="mt-8 grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1  gap-4">
-        {productList.length >0 && productList.map((product) => (
-          <MenuItem key={product._id} product={product} />
-        ))}
+      
+      <div className="mt-8 grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4 min-h-[450px]">
+        {filter.length > 0 &&
+          filter.map((product) => (
+            <MenuItem key={product._id} product={product} />
+          ))}
       </div>
     </div>
   );
