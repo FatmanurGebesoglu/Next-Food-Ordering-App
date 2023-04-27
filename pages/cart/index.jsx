03e46f1f5bae57c5208feb31c357dbb92cc1a366
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-04-27 20:24:02 +0300

    Product component edited

--- pages/cart/index.jsx
+++ pages/cart/index.jsx
@@ -1,38 +1,44 @@
-import { Title } from "@/components/ui/Title";
 import Image from "next/image";
-import React from "react";
+import {Title} from "../../components/ui/Title";
 import { useSelector, useDispatch } from "react-redux";
-import { reset } from "@/redux/cartSlice";
-
+import { reset } from "../../redux/cartSlice";
 const Cart = () => {
   const cart = useSelector((state) => state.cart);
   const dispatch = useDispatch();
   return (
-    <div className="min-h-[calc(100vh_-_433px)]" >
+    <div className="min-h-[calc(100vh_-_433px)]">
       <div className="flex justify-between items-center md:flex-row flex-col">
-      <div className="min-h-[calc(100vh_-_433px)] flex items-center flex-1 p-10 overflow-x-auto w-fu">
-        <table className="w-full txt-sm text-center text-gray-500 min-w-[1000px]">
-          <thead className="text-xs text-gray-400 uppercase bg-gray-700">
-            <tr>
-              <th scope="col" className="py-3 px-6" >PRODUCT</th>
-              <th className="py-3 px-6">EXTRAS</th>
-              <th className="py-3 px-6">PRICE</th>
-              <th className="py-3 px-6">QUANTITY</th>
-            </tr>
-          </thead>
-          <tbody>
-          {cart.products.map((product) => (
+        <div className="md:min-h-[calc(100vh_-_433px)] flex items-center flex-1 p-10 overflow-x-auto w-full">
+          <table className="w-full text-sm text-center text-gray-500 min-w-[1000px]">
+            <thead className="text-xs text-gray-400 uppercase bg-gray-700">
+              <tr>
+                <th scope="col" className="py-3 px-6">
+                  PRODUCT
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  EXTRAS
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  PRICE
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  QUANTITY
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {cart.products.map((product) => (
                 <tr
                   className="transition-all bg-secondary border-gray-700 hover:bg-primary"
                   key={product.id}
                 >
                   <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white flex items-center gap-x-1 justify-center">
-                    <Image src="/images/pizza.png" alt="" width={50} height={50} />
+                    <Image src={product.img} alt="" width={50} height={50} />
                     <span>{product.name}</span>
                   </td>
                   <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">
-                    {product.options.map((item) => (
-                      <span key={item.id}>{item.name}, </span>
+                    {product.extras.map((item) => (
+                      <span key={item.id}>{item.text}, </span>
                     ))}
                   </td>
                   <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">
@@ -43,24 +49,27 @@ const Cart = () => {
                   </td>
                 </tr>
               ))}
-          </tbody>
-        </table>
-      </div>
-      <div className="bg-secondary min-h-[calc(100vh_-_433px)] flex flex-col justify-center text-white p-12 md:w-auto w-full md:text-start !text-center">
-        <Title addclass="text-[40px] " >CART TOTAL</Title>
-        <div className="mt-6">
+            </tbody>
+          </table>
+        </div>
+        <div className="bg-secondary min-h-[calc(100vh_-_433px)] flex flex-col justify-center text-white p-12 md:w-auto w-full   md:text-start !text-center">
+          <Title addclass="text-[40px]">CART TOTAL</Title>
+          <div className="mt-6">
             <b>Subtotal: </b>${cart.total} <br />
             <b className=" inline-block my-1">Discount: </b>$0.00 <br />
-            <b>Total: </b>${cart.total} <br />
+            <b>Total: </b>${cart.total}
+          </div>
+          <div>
+            <button
+              className="btn-primary mt-4 md:w-auto w-52"
+              onClick={() => dispatch(reset())}
+            >
+              CHECKOUT NOW!
+            </button>
+          </div>
         </div>
-        <div>
-        <button className="btn-primary mt-4 md:w-auto w-52"
-              onClick={() => dispatch(reset())} >Checkout Now</button>
-        </div>
-      </div>
       </div>
     </div>
   );
 };
-
 export default Cart;
