2fc967c4d126399be3919f037641454843e5fd9a
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-04-30 15:56:10 +0300

    view more button added menuWrapper component

--- components/product/MenuWrapper.jsx
+++ components/product/MenuWrapper.jsx
@@ -6,6 +6,7 @@ import { useState, useEffect } from "react";
 const MenuWrapper = ({ categoryList, productList }) => {
   const [active, setActive] = useState(0);
   const [filter, setFilter] = useState([]);
+  const [productLimit, setProductLimit] = useState(3);
 
   useEffect(() => {
     setFilter(
@@ -15,7 +16,6 @@ const MenuWrapper = ({ categoryList, productList }) => {
       )
     );
   }, [categoryList, productList, active]);
-  
 
   return (
     <div className="container mx-auto  mb-16">
@@ -29,19 +29,30 @@ const MenuWrapper = ({ categoryList, productList }) => {
                   index === active && "bg-secondary text-white"
                 }`}
                 key={category._id}
-                onClick={() => setActive(index)}
+                onClick={() => {
+                  setActive(index);
+                  setProductLimit(3);
+                }}
               >
                 {category.title}
               </button>
             ))}
         </div>
       </div>
-      
+
       <div className="mt-8 grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4 min-h-[450px]">
         {filter.length > 0 &&
-          filter.map((product) => (
-            <MenuItem key={product._id} product={product} />
-          ))}
+          filter
+            .slice(0, productLimit)
+            .map((product) => <MenuItem key={product._id} product={product} />)}
+      </div>
+      <div className="flex items-center justify-center w-full mt-8">
+        <button
+          className="btn-primary"
+          onClick={() => setProductLimit(productLimit + 3)}
+        >
+          View More
+        </button>
       </div>
     </div>
   );
