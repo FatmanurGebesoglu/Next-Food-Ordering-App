60e8d916ea584130978e3f5c71c5c450feb96baa
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-04-30 16:01:10 +0300

    MenuItem component edited

--- components/product/MenuItem.jsx
+++ components/product/MenuItem.jsx
@@ -2,27 +2,45 @@ import Image from "next/image";
 import React from "react";
 import { FaShoppingCart } from "react-icons/fa";
 import Link from "next/link";
+import { useSelector, useDispatch } from "react-redux";
+import { addProduct } from "../../redux/cartSlice";
 
-const MenuItem = ({product}) => {
+const MenuItem = ({ product }) => {
+  const cart = useSelector((state) => state.cart);
+  const findCart = cart.products.find((item) => item._id === product._id);
+
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(
+      addProduct({
+        ...product,
+        extras: [{ text: "empty" }],
+        price: product.prices[0],
+        quantity: 1,
+      })
+    );
+  };
+
   return (
     <div className="bg-secondary rounded-3xl">
       <div className="w-full  bg-[#f1f2f3] h-[210px] grid place-content-center rounded-bl-[46px] rounded-tl-2xl rounded-tr-2xl">
-        <Link href= {`/product/${product._id}`} >
+        <Link href={`/product/${product._id}`}>
           <div className="relative w-36 h-36 hover:scale-105 transition-all ">
-            <Image src={product.img} alt="" fill
-            priority="true"
-            />
+            <Image src={product.img} alt="" fill priority="true" />
           </div>
         </Link>
       </div>
       <div className="p-[25px] text-white">
         <h4 className="text-xl font-semibold">{product.title}</h4>
-        <p className="text-[15px]">
-          {product.desc} 
-        </p>
+        <p className="text-[15px]">{product.desc}</p>
         <div className="flex justify-between items-center mt-4 ">
           <span>${product.prices[0]}</span>
-          <button className="btn-primary !w-10 !h-10 !rounded-full !p-0 !grid !place-content-center">
+          <button
+            className="btn-primary !w-10 !h-10 !rounded-full !p-0 grid place-content-center"
+            disabled={findCart}
+            onClick={handleClick}
+          >
             <FaShoppingCart />
           </button>
         </div>
